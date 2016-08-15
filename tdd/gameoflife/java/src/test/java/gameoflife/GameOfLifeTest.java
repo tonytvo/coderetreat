b0d6653ec60d39e75ae7ce36d4d922dcce0f6b2a
Tony Vo <tony.vo@calgaryscientific.com>
2016-08-14 21:54:43 -0600

    added todo to gameoflife test

--- tdd/gameoflife/java/src/test/java/gameoflife/GameOfLifeTest.java
+++ tdd/gameoflife/java/src/test/java/gameoflife/GameOfLifeTest.java
@@ -8,6 +8,12 @@ import org.junit.Test;
  */
 public class GameOfLifeTest {
 
+    // TODO
+    // Any live cell with fewer than two live neighbours dies, as if caused by under-population
+    // Any live cell with two or three live neighbours lives on to the next generation.
+    // Any live cell with more than three live neighbours dies, as if by over-population.
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
     @Test
     public void sampleTest() throws Exception {
         Assert.assertTrue("First dummy test", false);
