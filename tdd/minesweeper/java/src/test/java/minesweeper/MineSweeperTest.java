f83db18b322f2dbe6a0f0f3654115eae19b4c9a7
Tony Vo <tony.vo@calgaryscientific.com>
2016-08-26 22:17:05 -0600

    added todo to the test

--- tdd/minesweeper/java/src/test/java/minesweeper/MineSweeperTest.java
+++ tdd/minesweeper/java/src/test/java/minesweeper/MineSweeperTest.java
@@ -7,6 +7,19 @@ import org.junit.Test;
  * Created by Tony on 1/15/2016.
  */
 public class MineSweeperTest {
+    // TODO
+    // Input
+    // 44
+    // *...
+    // ....
+    // .*..
+    // ....
+    // Output
+    // Field #1:
+    // *100
+    // 2210
+    // 1*10
+    // 1110
 
     @Test
     public void sampleTest() throws Exception {
