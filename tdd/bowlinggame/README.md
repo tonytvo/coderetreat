e80d8601028f7cdbb312783bc869f61347d8a7c0
Tony Vo <tony.vo@calgaryscientific.com>
2016-08-27 11:32:29 -0600

    added bowling game

---
+++ tdd/bowlinggame/README.md
@@ -0,0 +1,23 @@
+# Bowling Game
+
+* The Code Dojo: https://www.youtube.com/watch?v=OPGTPQ4kURU
+
+# Usage
+
+The purpose of this is to provide good examples for Bowling Game TDD kata.
+
+Build
+-----
+
+All you need to build this project is Java 8.0 (Java SDK 1.8) or later, Gradle.
+
+Testing
+-------
+
+Unit tests can be run using gradle [1]:
+
+    $ gradle test
+
+[1]: http://gradle.org/
+
+Tests are located in the test directory and run using Junit.
---
+++ tdd/bowlinggame/java/src/test/java/minesweeper/MineSweeperTest.java
@@ -0,0 +1,28 @@
+package minesweeper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Tony on 1/15/2016.
+ */
+public class MineSweeperTest {
+    // TODO
+    // Input
+    // 44
+    // *...
+    // ....
+    // .*..
+    // ....
+    // Output
+    // Field #1:
+    // *100
+    // 2210
+    // 1*10
+    // 1110
+
+    @Test
+    public void sampleTest() throws Exception {
+        Assert.assertTrue("First dummy test", false);
+    }
+}
