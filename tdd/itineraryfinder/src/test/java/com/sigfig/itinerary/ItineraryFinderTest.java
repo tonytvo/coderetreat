84ead19685934690b2482f7af18c60c897896cc2
Tony Vo <tony.vo@calgaryscientific.com>
2017-08-16 00:03:16 -0600

    cleaned up starting test

---
+++ tdd/itineraryfinder/src/test/java/com/sigfig/itinerary/ItineraryFinderTest.java
@@ -0,0 +1,11 @@
+package com.sigfig.itinerary;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ItineraryFinderTest {
+    @Test
+    public void firstTest() throws Exception {
+        Assert.assertTrue(false);
+    }
+}
