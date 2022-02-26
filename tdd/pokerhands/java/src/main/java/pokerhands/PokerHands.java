c463e50fccf44aa976b969073904e8c26916f877
Tony Trung Thanh Vo <ttrung.vo@gmail.com>
2022-02-26 08:50:10 -0700

    Delete tdd/pokerhands directory

--- tdd/pokerhands/java/src/main/java/pokerhands/PokerHands.java
+++
@@ -1,5 +0,0 @@
-package pokerhands;
-
-public class PokerHands {
-
-}
--- tdd/pokerhands/java/src/main/java/pokerhands/model/Suit.java
+++
@@ -1,8 +0,0 @@
-package pokerhands.model;
-
-/**
- * Created by tvo on 8/27/16.
- */
-public enum Suit {
-    HEART, DIAMOND, SPADE, CLUB
-}
