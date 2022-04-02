8fa15387d589205990c86d30ce1cc772e63f3823
Tony Vo <ttrung.vo@gmail.com>
2022-04-02 15:39:16 -0600

    r remove unused method

--- refactoring/gildedrose/js/app/gilded-rose.ts
+++ refactoring/gildedrose/js/app/gilded-rose.ts
@@ -89,32 +89,6 @@ export class EveryThingQualityUpdater implements QualityUpdater {
             item.quality = item.quality - 1
         }
     }
-
-    private updateQualityBackstagePasses(item: Item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-            if (item.sellIn < 11) {
-                this.increaseQualityIfLessThan50(item);
-            }
-            if (item.sellIn < 6) {
-                this.increaseQualityIfLessThan50(item);
-            }
-        }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-            this.resetQuality(item);
-        }
-    }
-
-    private resetQuality(item: Item) {
-        item.quality = item.quality - item.quality
-    }
-
-    private increaseQualityIfLessThan50(item: Item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-        }
-    }
 }
 
 export class GildedRose {
