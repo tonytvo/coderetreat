8b67d9a81f647c01c6449e41d9385c9a3a4f73c0
Tony Vo <tony.vo@shareworks.com>
2022-04-02 15:09:42 -0600

    r extract method updateQualtyForEverythingElse

--- refactoring/gildedrose/js/app/gilded-rose.ts
+++ refactoring/gildedrose/js/app/gilded-rose.ts
@@ -37,23 +37,27 @@ export class GildedRose {
       if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
         this.updateQualityBackstagePasses(item);
       } else {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
+        this.updateQualityForEverythingElse(item);
+      }
+    }
+
+  private updateQualityForEverythingElse(item: Item) {
+    if (item.quality > 0) {
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.quality = item.quality - 1
+      }
+    }
+    if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.quality > 0) {
         if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.sellIn = item.sellIn - 1;
-        }
-        if (item.sellIn < 0) {
-          if (item.quality > 0) {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              item.quality = item.quality - 1
-            }
-          }
+          item.quality = item.quality - 1
         }
       }
     }
+  }
 
   private updateQualityBackstagePasses(item: Item) {
     if (item.quality < 50) {
