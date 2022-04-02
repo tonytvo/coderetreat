e685651c19f48177798b91d7424bd31b520e0edc
Tony Vo <tony.vo@shareworks.com>
2022-04-02 14:36:08 -0600

    t use toMatchSnapshot

--- refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
+++ refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
@@ -5,6 +5,6 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     let result = items[0].name;
-    expect(result).toBe('foo');
+    expect(result).toMatchSnapshot('foo');
   });
 });
