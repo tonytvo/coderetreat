ff89aef249f46c45a85994a65364a9c7853d12be
Tony Vo <tony.vo@shareworks.com>
2022-04-02 14:36:54 -0600

    r extract method

--- refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
+++ refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
@@ -1,10 +1,15 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
+  function updateGildedRoseQuality() {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     let result = items[0].name;
+    return result;
+  }
+
+  it('should foo', () => {
+    let result = updateGildedRoseQuality();
     expect(result).toMatchSnapshot('foo');
   });
 });
