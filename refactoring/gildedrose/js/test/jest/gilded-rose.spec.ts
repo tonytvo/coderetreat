dd3e8c51a75654e0df6b8a13ec71d57c54d78ada
Tony Vo <tony.vo@shareworks.com>
2022-04-02 14:35:32 -0600

    r introduce variables

--- refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
+++ refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
@@ -4,6 +4,7 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+    let result = items[0].name;
+    expect(result).toBe('foo');
   });
 });
