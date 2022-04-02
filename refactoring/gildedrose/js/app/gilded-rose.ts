2efb3f034bdee75401bf792f07344b70acaaba67
Tony Vo <tony.vo@shareworks.com>
2022-04-02 15:14:48 -0600

    R flip condition for sulfurus

--- refactoring/gildedrose/js/app/gilded-rose.ts
+++ refactoring/gildedrose/js/app/gilded-rose.ts
@@ -34,22 +34,21 @@ export class GildedRose {
         this.updateQualityBackstagePasses(item);
         return;
       }
-      this.updateQualityForEverythingElse(item);
-    }
-  private updateQualityForEverythingElse(item: Item) {
-    if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.quality = item.quality - 1
+      if (item.name == 'Sulfuras, Hand of Ragnaros') {
+        return;
       }
+
+      this.updateQualityForDefaultItem(item);
     }
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-      item.sellIn = item.sellIn - 1;
+
+  private updateQualityForDefaultItem(item: Item) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
     }
+    item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0) {
       if (item.quality > 0) {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.quality = item.quality - 1
-        }
+        item.quality = item.quality - 1
       }
     }
   }
