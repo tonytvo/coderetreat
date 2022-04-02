8ba6dcb0e3195c906e8bc4776fed27e29aacda63
Tony Vo <tony.vo@shareworks.com>
2022-04-02 14:37:54 -0600

    r introduce parameter

--- refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
+++ refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
@@ -1,14 +1,14 @@
 import {GildedRose, Item} from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  function updateGildedRoseQuality() {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  function updateGildedRoseQuality(name: string) {
+    const gildedRose = new GildedRose([new Item(name, 0, 0)]);
     const items = gildedRose.updateQuality();
     let result = items[0].name;
     return result;
   }
 
   it('should foo', () => {
-    expect(updateGildedRoseQuality()).toMatchSnapshot('foo');
+    expect(updateGildedRoseQuality('foo')).toMatchSnapshot('foo');
   });
 });
