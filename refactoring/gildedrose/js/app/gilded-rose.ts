267b3bc1f17e792037fe93c788ea70c7ff8e49f1
Tony Vo <ttrung.vo@gmail.com>
2022-04-02 15:19:53 -0600

    r extrace method

--- refactoring/gildedrose/js/app/gilded-rose.ts
+++ refactoring/gildedrose/js/app/gilded-rose.ts
@@ -79,9 +79,13 @@ export class GildedRose {
         }
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
+            this.increaseQualityIfLessThan50(item);
+        }
+    }
+
+    private increaseQualityIfLessThan50(item: Item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
         }
     }
 }
