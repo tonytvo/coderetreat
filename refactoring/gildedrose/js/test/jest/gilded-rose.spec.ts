6e4b1fb58f280343e86e47421021a8bcf6190497
Tony Vo <tony.vo@shareworks.com>
2022-04-02 14:42:56 -0600

    t add more names

--- refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
+++ refactoring/gildedrose/js/test/jest/gilded-rose.spec.ts
@@ -9,7 +9,12 @@ describe('Gilded Rose', () => {
   }
 
   it('should foo', () => {
-    let names = ['foo'];
+    let names = ['foo'
+      ,"+5 Dexterity Vest"
+      ,"Aged Brie"
+      ,"Elixir of the Mongoose"
+      ,"Sulfuras, Hand of Ragnaros"
+      ,"Backstage passes to a TAFKAL80ETC concert"];
     expect(updateGildedRoseQuality).toVerifyAllCombinations(names);
   });
 });
