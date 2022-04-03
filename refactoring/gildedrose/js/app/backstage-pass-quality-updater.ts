3f61b3d295cebf12e2e3dcb68d852375e8478451
Tony Vo <ttrung.vo@gmail.com>
2022-04-02 19:11:59 -0600

    R reuse item.increaseQuanlity

--- refactoring/gildedrose/js/app/backstage-pass-quality-updater.ts
+++ refactoring/gildedrose/js/app/backstage-pass-quality-updater.ts
@@ -7,14 +7,12 @@ export class BackstagePassQualityUpdater implements QualityUpdater {
     }
 
     private updateQualityBackstagePasses(item: Item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-            if (item.sellIn < 11) {
-                item.increaseQuality();
-            }
-            if (item.sellIn < 6) {
-                item.increaseQuality();
-            }
+        item.increaseQuality();
+        if (item.sellIn < 11) {
+            item.increaseQuality();
+        }
+        if (item.sellIn < 6) {
+            item.increaseQuality();
         }
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
