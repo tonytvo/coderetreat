3b81edd9118dafb9af6fe49054644db081647281
Tony Vo <ttrung.vo@gmail.com>
2022-04-03 10:24:21 -0600

    player can damanage non players like tree

--- tdd/rpg-combat/java/src/main/java/org/rpg/AttackActionFactory.java
+++ tdd/rpg-combat/java/src/main/java/org/rpg/AttackActionFactory.java
@@ -2,12 +2,32 @@ package org.rpg;
 
 public class AttackActionFactory {
     public AttackAction createAttackAction(Player attackingPlayer,
-                                           Player target,
+                                           Target target,
                                            DistanceCalculator distanceCalculator,
                                            int damage) {
+
+        if (target instanceof Thing) {
+            return new AttackThingAction(target, damage);
+        }
+
         if (target instanceof Player) {
-            return new PlayerAttackAction(attackingPlayer, target, distanceCalculator, damage);
+            return new PlayerAttackAction(attackingPlayer, (Player) target, distanceCalculator, damage);
         }
         throw new UnsupportedOperationException("not implemented yet");
+    }
+
+    private static class AttackThingAction implements AttackAction {
+        private final Target target;
+        private final int damage;
+
+        public AttackThingAction(Target target, int damage) {
+            this.target = target;
+            this.damage = damage;
+        }
+
+        @Override
+        public void attack() {
+            target.receiveDamage(damage);
+        }
     }
 }
